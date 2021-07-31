70a6dea14ee7a00447a96a69a54d43025044a679
Rizavur Rahman <rizavur@gmail.com>
2021-07-31 15:54:34 +0800

    Clean up code

--- src/screens/configurations.tsx
+++ src/screens/configurations.tsx
@@ -1,81 +1,64 @@
-import { useState } from "react";
-import { Row } from "react-bootstrap";
-import CurrenciesModal from "./configurationsModals/currencies/currenciesModal";
-import CurrenciesView from "./configurationsModals/currencies/currenciesView";
-import CustomersModal from "./configurationsModals/customers/customersModal";
-import CustomersView from "./configurationsModals/customers/customersView";
-import DealersModal from "./configurationsModals/dealers/dealersModal";
-import DealersView from "./configurationsModals/dealers/dealersView";
-import { Currencies } from "./configurationsModals/types";
+import { useState } from 'react'
+import { Row } from 'react-bootstrap'
+import CurrenciesModal from './configurationsModals/currencies/currenciesModal'
+import CurrenciesView from './configurationsModals/currencies/currenciesView'
+import CustomersModal from './configurationsModals/customers/customersModal'
+import CustomersView from './configurationsModals/customers/customersView'
+import { Currencies } from './configurationsModals/types'
 
 const Configurations = () => {
-	const [showCurrenciesModal, setShowCurrenciesModal] = useState(false);
-	const [showCustomersModal, setShowCustomersModal] = useState(false);
-	const [showDealersModal, setShowDealersModal] = useState(false);
+  const [showCurrenciesModal, setShowCurrenciesModal] = useState(false)
+  const [showCustomersModal, setShowCustomersModal] = useState(false)
 
-	const [currenciesList, setCurrenciesList] = useState<Currencies[]>([]);
+  const [currenciesList, setCurrenciesList] = useState<Currencies[]>([])
 
-	const handleClose = (type: string) => {
-		switch (type) {
-			case "currency":
-				setShowCurrenciesModal(false);
-				break;
-			case "customer":
-				setShowCustomersModal(false);
-				break;
-			case "dealer":
-				setShowDealersModal(false);
-				break;
-			default:
-				break;
-		}
-	};
+  const handleClose = (type: string) => {
+    switch (type) {
+      case 'currency':
+        setShowCurrenciesModal(false)
+        break
+      case 'customer':
+        setShowCustomersModal(false)
+        break
+      default:
+        break
+    }
+  }
 
-	const handleShow = (type: string) => {
-		switch (type) {
-			case "currency":
-				setShowCurrenciesModal(true);
-				break;
-			case "customer":
-				setShowCustomersModal(true);
-				break;
-			case "dealer":
-				setShowDealersModal(true);
-				break;
-			default:
-				break;
-		}
-	};
+  const handleShow = (type: string) => {
+    switch (type) {
+      case 'currency':
+        setShowCurrenciesModal(true)
+        break
+      case 'customer':
+        setShowCustomersModal(true)
+        break
+      default:
+        break
+    }
+  }
 
-	return (
-		<>
-			<h1 style={{ marginTop: 20, marginLeft: 20, fontWeight: 550 }}>
-				Configurations
-			</h1>
-			<CurrenciesView
-				handleShow={handleShow}
-				currenciesList={currenciesList}
-			/>
-			<Row style={{ marginTop: 20, marginRight: 10, marginLeft: 10 }}>
-				<CustomersView handleShow={handleShow} />
-				<DealersView handleShow={handleShow} />
-			</Row>
-			<CurrenciesModal
-				handleClose={handleClose}
-				showModal={showCurrenciesModal}
-				setCurrenciesList={setCurrenciesList}
-				currenciesList={currenciesList}
-			/>
-			<CustomersModal
-				handleClose={handleClose}
-				showModal={showCustomersModal}
-			/>
-			<DealersModal
-				handleClose={handleClose}
-				showModal={showDealersModal}
-			/>
-		</>
-	);
-};
+  return (
+    <>
+      <h1 style={{ marginTop: 20, marginLeft: 20, fontWeight: 550 }}>
+        Configurations
+      </h1>
+      <CurrenciesView handleShow={handleShow} currenciesList={currenciesList} />
+      <Row style={{ marginTop: 20, marginRight: 10, marginLeft: 10 }}>
+        <CustomersView handleShow={handleShow} />
+      </Row>
+      <CurrenciesModal
+        handleClose={handleClose}
+        showModal={showCurrenciesModal}
+        setCurrenciesList={setCurrenciesList}
+        currenciesList={currenciesList}
+      />
+      <CustomersModal
+        handleClose={handleClose}
+        showModal={showCustomersModal}
+      />
+    </>
+  )
+}
 
-export default Configurations;
+export default Configurations
--- src/screens/configurationsModals/customers/customersView.tsx
+++ src/screens/configurationsModals/customers/customersView.tsx
@@ -1,93 +1,91 @@
-import React from "react";
-import { Row, Col, Card, Button, Table } from "react-bootstrap";
-import { AiOutlineEdit, AiFillDelete } from "react-icons/ai";
-import * as customers from "../../../dummydata/customers";
-import { ViewInputParams } from "../types";
+import React from 'react'
+import { Row, Col, Card, Button, Table } from 'react-bootstrap'
+import { AiOutlineEdit, AiFillDelete } from 'react-icons/ai'
+import { ViewInputParams } from '../types'
 
 const CustomersView = ({ handleShow }: ViewInputParams) => {
-	return (
-		<Col>
-			<Card style={{ padding: 20 }}>
-				<Row>
-					<Col>
-						<h2 style={{ fontWeight: 400 }}>Customers</h2>
-					</Col>
-					<Col style={{ display: "flex" }}>
-						<Button
-							style={{
-								width: 50,
-								height: 50,
-								marginLeft: "auto",
-								backgroundColor: "black",
-								fontSize: 20,
-							}}
-							onClick={() => handleShow("customer")}
-						>
-							+
-						</Button>
-					</Col>
-				</Row>
-				<Row style={{ marginTop: 20, marginLeft: 5, marginRight: 5 }}>
-					<Table striped bordered hover responsive>
-						<thead>
-							<tr>
-								<th>#</th>
-								<th>Name</th>
-								<th style={{ width: 50 }}>Edit</th>
-								<th style={{ width: 70 }}>Delete</th>
-							</tr>
-						</thead>
-						<tbody>
-							{customers.CUSTOMERS.map((item, index) => {
-								return (
-									<tr>
-										<td>{index + 1}</td>
-										<td>{item.name}</td>
-										<td>
-											<Row
-												style={{
-													paddingLeft: 20,
-													paddingRight: 20,
-												}}
-											>
-												<Button
-													size="sm"
-													style={{
-														backgroundColor:
-															"black",
-													}}
-												>
-													<AiOutlineEdit />
-												</Button>
-											</Row>
-										</td>
-										<td>
-											<Row
-												style={{
-													paddingLeft: 20,
-													paddingRight: 20,
-												}}
-											>
-												<Button
-													size="sm"
-													style={{
-														backgroundColor:
-															"black",
-													}}
-												>
-													<AiFillDelete />
-												</Button>
-											</Row>
-										</td>
-									</tr>
-								);
-							})}
-						</tbody>
-					</Table>
-				</Row>
-			</Card>
-		</Col>
-	);
-};
+  const customers = { CUSTOMERS: [{ name: 'test' }] }
+  return (
+    <Col>
+      <Card style={{ padding: 20 }}>
+        <Row>
+          <Col>
+            <h2 style={{ fontWeight: 400 }}>Customers</h2>
+          </Col>
+          <Col style={{ display: 'flex' }}>
+            <Button
+              style={{
+                width: 50,
+                height: 50,
+                marginLeft: 'auto',
+                backgroundColor: 'black',
+                fontSize: 20,
+              }}
+              onClick={() => handleShow('customer')}
+            >
+              +
+            </Button>
+          </Col>
+        </Row>
+        <Row style={{ marginTop: 20, marginLeft: 5, marginRight: 5 }}>
+          <Table striped bordered hover responsive>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th style={{ width: 50 }}>Edit</th>
+                <th style={{ width: 70 }}>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {customers.CUSTOMERS.map((item, index) => {
+                return (
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>{item.name}</td>
+                    <td>
+                      <Row
+                        style={{
+                          paddingLeft: 20,
+                          paddingRight: 20,
+                        }}
+                      >
+                        <Button
+                          size="sm"
+                          style={{
+                            backgroundColor: 'black',
+                          }}
+                        >
+                          <AiOutlineEdit />
+                        </Button>
+                      </Row>
+                    </td>
+                    <td>
+                      <Row
+                        style={{
+                          paddingLeft: 20,
+                          paddingRight: 20,
+                        }}
+                      >
+                        <Button
+                          size="sm"
+                          style={{
+                            backgroundColor: 'black',
+                          }}
+                        >
+                          <AiFillDelete />
+                        </Button>
+                      </Row>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </Table>
+        </Row>
+      </Card>
+    </Col>
+  )
+}
 
-export default CustomersView;
+export default CustomersView
