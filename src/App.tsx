2ca88c789498aa660ea38d7e01668751a9aecac5
Rizavur Rahman <rizavur@gmail.com>
2022-01-06 21:30:37 +0800

    Cleanup react-bootstrap dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import './App.css'
 import 'antd/dist/antd.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css'
-import 'react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css'
 import { AppNavigator } from './AppNavigator'
 import { GlobalProvider } from './Providers/GlobalProvider'
 
